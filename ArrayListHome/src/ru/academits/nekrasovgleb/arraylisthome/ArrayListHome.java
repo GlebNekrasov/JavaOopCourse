df1281a90d523621937f597c90155447cdbd8a02
GlebNekrasov <g_nekrasov@mail.ru>
2022-03-17 22:10:40 +0700

    1. Добавлена обработка исключения IOException, чтобы программа не падала в случае проблемы с чтением файла. 2. Наименования переменных изменены на более подходящие. 3. Для чтения строк из файла использован BufferedReader вместо Scanner. 4. В последней подзадаче вместо изменения исходного списка создан новый список, который отвечает условиям задачи.

--- ArrayListHome/src/ru/academits/nekrasovgleb/arraylisthome/ArrayListHome.java
+++ ArrayListHome/src/ru/academits/nekrasovgleb/arraylisthome/ArrayListHome.java
@@ -1,49 +1,52 @@
 package ru.academits.nekrasovgleb.arraylisthome;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Scanner;
 
 public class ArrayListHome {
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) {
         // 1. Чтение в список всех строк файла
-        try (Scanner scanner = new Scanner(new FileInputStream("Strings.txt"))) {
-            ArrayList<String> stringsList = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader("Strings.txt"))) {
+            ArrayList<String> linesList = new ArrayList<>();
 
-            while (scanner.hasNextLine()) {
-                stringsList.add(scanner.nextLine());
+            for (String line = reader.readLine(); line != null; ) {
+                linesList.add(line);
+                line = reader.readLine();
             }
 
             System.out.println("Проверяем, что строки файла добавились в список:");
-            System.out.println(stringsList);
+            System.out.println(linesList);
             System.out.println();
+        } catch (IOException e) {
+            System.out.println("При попытке прочитать файл произошла ошибка: " + e.getMessage());
         }
 
         // 2. Удаление четных чисел из списка целых чисел
-        ArrayList<Integer> integersList = new ArrayList<>(Arrays.asList(2, 7, 4, 2, 5, 5, 6, 9, 4, 7, 8, 13, 14, 15, 2));
-        System.out.println("Исходный список: " + integersList);
+        ArrayList<Integer> numbersList1 = new ArrayList<>(Arrays.asList(2, 7, 4, 2, 5, 5, 6, 9, 4, 7, 8, 13, 14, 15, 2));
+        System.out.println("Исходный список: " + numbersList1);
 
-        for (int i = integersList.size() - 1; i >= 0; --i) {
-            if (integersList.get(i) % 2 == 0) {
-                integersList.remove(i);
+        for (int i = numbersList1.size() - 1; i >= 0; --i) {
+            if (numbersList1.get(i) % 2 == 0) {
+                numbersList1.remove(i);
             }
         }
 
-        System.out.println("Список после удаления четных чисел: " + integersList);
+        System.out.println("Список после удаления четных чисел: " + numbersList1);
         System.out.println();
 
         // 3. Создание нового списка без повторений
-        ArrayList<Integer> integersList2 = new ArrayList<>(Arrays.asList(2, 7, 4, 2, 5, 5, 6, 9, 4, 7, 8, 13, 14, 15, 2));
-        System.out.println("Исходный список: " + integersList2);
+        ArrayList<Integer> numbersList2 = new ArrayList<>(Arrays.asList(2, 7, 4, 2, 5, 5, 6, 9, 4, 7, 8, 13, 14, 15, 2));
+        System.out.println("Исходный список: " + numbersList2);
 
-        for (int i = integersList2.size() - 1; i >= 0; --i) {
-            if (integersList2.indexOf(integersList2.get(i)) != i) {
-                integersList2.remove(i);
+        ArrayList<Integer> numbersList3 = new ArrayList<>();
+
+        for (int i = 0; i < numbersList2.size(); ++i) {
+            if (numbersList2.indexOf(numbersList2.get(i)) == i) {
+                numbersList3.add(numbersList2.get(i));
             }
         }
 
-        System.out.println("Список после удаления повторяющихся элементов: " + integersList2);
+        System.out.println("Список после удаления повторяющихся элементов: " + numbersList3);
     }
 }
