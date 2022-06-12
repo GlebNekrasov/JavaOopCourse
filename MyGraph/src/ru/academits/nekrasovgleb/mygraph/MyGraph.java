2dd36f5c5de8c3ba240e131f186324c6c4b0f0c6
GlebNekrasov <g_nekrasov@mail.ru>
2022-06-12 23:40:39 +0700

    1. Оптимизированы поля класса MyGraph: - в поле edges теперь используется int вместо Integer. - поле verticesData переименовано в vertices. - полу visitedVertices удалено, вместо него в методах теперь используется локальная переменная visited. 2. Исключения в конструкторе сделаны более информативными. 3. В методах переменная currentVertex переименована в vertexIndex. 4. В методах обхода графа теперь не проверяется, есть ли вершина в очереди или в стеке - вершина добавляется, даже если она уже есть в очереди или в стеке. Вместо этого добавлена проверка наличия индекса вершины в переменной visited перед передачей вершины в consumer. 5. В методе обхода в глубину исправлен порядок обхода вершин. 6. Добавлен метод обхода в глубину с рекурсией. 7. В Main матрица связей теперь объявлена кратким синтаксисом.

--- MyGraph/src/ru/academits/nekrasovgleb/mygraph/MyGraph.java
+++ MyGraph/src/ru/academits/nekrasovgleb/mygraph/MyGraph.java
@@ -7,19 +7,25 @@ import java.util.Queue;
 import java.util.function.Consumer;
 
 public class MyGraph<T> {
-    private final T[] verticesData;
-    private final Integer[][] edges;
-    private final boolean[] visitedVertices;
+    private final T[] vertices;
+    private final int[][] edges;
 
-    public MyGraph (T[] verticesData, Integer[][] edges) {
-        if (verticesData == null || verticesData.length == 0) {
-            throw new IllegalArgumentException("При создании графа в качестве аргумента передан пустой массив данных, " +
-                    "которые содержат вершины графа. Массив данных для вершин графа должен содержать данные как минимум для одной вершины.");
+    public MyGraph(T[] vertices, int[][] edges) {
+        if (vertices == null) {
+            throw new NullPointerException("При создании графа передан объект null в качестве аргумента, который должен " +
+                    "содержать данные вершин графа. Этот аргумент должен содержать данные как минимум для одной вершины.");
         }
 
-        if (edges.length != verticesData.length) {
-            throw new IllegalArgumentException("При создании графа передан массив массивов, описывающий наличие ребер между вершинами, " +
-                    "у которого длина не совпадает с количеством вершин в графе. Длина массива массивов должна быть равна количеству вершин.");
+        if (vertices.length == 0) {
+            throw new IllegalArgumentException("При создании графа передан массив длины 0 в качестве аргумента, " +
+                    "который должен содержать данные вершин графа. Массив данных для вершин графа должен содержать данные " +
+                    "как минимум для одной вершины.");
+        }
+
+        if (edges.length != vertices.length) {
+            throw new IllegalArgumentException("При создании графа передан массив длины " + vertices.length +
+                    ", который содержит данные вершин графа, и передан массив массивов длины " + edges.length +
+                    ", который описывает наличие ребер между вершинами. Длины этих массивов должны быть равны.");
         }
 
         if (!isEdgesMatrixSquare(edges)) {
@@ -27,13 +33,12 @@ public class MyGraph<T> {
                     " с разным количеством строк и столбцов. Количество строк и столбцов в массиве массивов должно совпадать.");
         }
 
-        this.verticesData = Arrays.copyOf(verticesData, verticesData.length);
+        this.vertices = Arrays.copyOf(vertices, vertices.length);
         this.edges = Arrays.copyOf(edges, edges.length);
-        visitedVertices = new boolean[edges.length];
     }
 
-    private boolean isEdgesMatrixSquare(Integer[][] edgesMatrix) {
-        for (Integer[] row : edgesMatrix) {
+    private boolean isEdgesMatrixSquare(int[][] edgesMatrix) {
+        for (int[] row : edgesMatrix) {
             if (row == null || row.length != edgesMatrix.length) {
                 return false;
             }
@@ -43,55 +48,77 @@ public class MyGraph<T> {
     }
 
     public void traverseByWidth(Consumer<T> consumer) {
-        Arrays.fill(visitedVertices, false);
+        final boolean[] visited = new boolean[edges.length];
 
         for (int i = 0; i < edges.length; ++i) {
-            if (!visitedVertices[i]) {
-                traverseVertexByWidth(i, consumer);
-            }
-        }
-    }
+            if (!visited[i]) {
+                Queue<Integer> queue = new LinkedList<>();
+                queue.add(i);
 
-    private void traverseVertexByWidth(Integer vertexNumber, Consumer<T> consumer) {
-        Queue<Integer> queue = new LinkedList<>();
-        queue.add(vertexNumber);
+                while (!queue.isEmpty()) {
+                    Integer vertexIndex = queue.remove();
 
-        while (!queue.isEmpty()) {
-            Integer currentVertex = queue.remove();
-            consumer.accept(verticesData[currentVertex]);
-            visitedVertices[currentVertex] = true;
+                    if (!visited[vertexIndex]) {
+                        consumer.accept(vertices[vertexIndex]);
+                        visited[vertexIndex] = true;
+                    }
 
-            for (int i = 0; i < edges.length; ++i) {
-                if (i != currentVertex && edges[currentVertex][i] != 0 && !visitedVertices[i] && !queue.contains(i)) {
-                    queue.add(i);
+                    for (int j = 0; j < edges.length; ++j) {
+                        if (j != vertexIndex && edges[vertexIndex][j] != 0 && !visited[j]) {
+                            queue.add(j);
+                        }
+                    }
                 }
             }
         }
     }
 
     public void traverseByDepth(Consumer<T> consumer) {
-        Arrays.fill(visitedVertices, false);
+        final boolean[] visited = new boolean[edges.length];
 
         for (int i = 0; i < edges.length; ++i) {
-            if (!visitedVertices[i]) {
-                traverseVertexByDepth(i, consumer);
+            if (!visited[i]) {
+                Deque<Integer> stack = new LinkedList<>();
+                stack.add(i);
+
+                while (!stack.isEmpty()) {
+                    Integer vertexIndex = stack.removeLast();
+
+                    if (!visited[vertexIndex]) {
+                        consumer.accept(vertices[vertexIndex]);
+                        visited[vertexIndex] = true;
+                    }
+
+                    for (int j = edges.length - 1; j >= 0; --j) {
+                        if (j != vertexIndex && edges[vertexIndex][j] != 0 && !visited[j]) {
+                            stack.addLast(j);
+                        }
+                    }
+                }
             }
         }
     }
 
-    private void traverseVertexByDepth(Integer vertexNumber, Consumer<T> consumer) {
-        Deque<Integer> stack = new LinkedList<>();
-        stack.add(vertexNumber);
+    // visit - вспомогательная функция для функции обхода графа в глубину с рекурсией
+    private void visit(int vertexIndex, boolean[] visited, Consumer<T> consumer) {
+        if (!visited[vertexIndex]) {
+            consumer.accept(vertices[vertexIndex]);
+            visited[vertexIndex] = true;
+        }
 
-        while (!stack.isEmpty()) {
-            Integer currentVertex = stack.removeLast();
-            consumer.accept(verticesData[currentVertex]);
-            visitedVertices[currentVertex] = true;
+        for (int i = 0; i < edges.length; ++i) {
+            if (i != vertexIndex && edges[vertexIndex][i] != 0 && !visited[i]) {
+                visit(i, visited, consumer);
+            }
+        }
+    }
 
-            for (int i = 0; i < edges.length; ++i) {
-                if (i != currentVertex && edges[currentVertex][i] != 0 && !visitedVertices[i] && !stack.contains(i)) {
-                    stack.addLast(i);
-                }
+    public void traverseByDepthRecursion(Consumer<T> consumer) {
+        final boolean[] visited = new boolean[edges.length];
+
+        for (int i = 0; i < vertices.length; ++i) {
+            if (!visited[i]) {
+                visit(i, visited, consumer);
             }
         }
     }
@@ -100,7 +127,7 @@ public class MyGraph<T> {
     public String toString() {
         StringBuilder stringBuilder = new StringBuilder();
 
-        for (Integer[] array : edges) {
+        for (int[] array : edges) {
             stringBuilder
                     .append(Arrays.toString(array))
                     .append(System.lineSeparator());
